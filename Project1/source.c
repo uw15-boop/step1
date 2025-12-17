ec596816bf322b3e2416abe22c066adec78512b0
uw15-boop <ujw1215@gmail.com>
2025-12-17 21:44:16 +0900

    (2025,12,17) 조건문

--- Project1/source.c
+++ Project1/source.c
@@ -60,24 +60,121 @@ void main()
 #pragma endregion
 
 #pragma region else문
-	// if문과 else if문의 조건이 다 틀리면
-	// 실행되는 명령문입니다.
+//	// if문과 else if문의 조건이 다 틀리면
+//	// 실행되는 명령문입니다.
+//
+//	int integer = 0;
+//
+//	if (integer > 0) {
+//		printf("positive number");
+//	}
+//	else if (integer < 0) {
+//		printf("negative quantity");
+//	}
+//	else
+//	{
+//		printf("integer");
+//	}
+//
+//	// if문에 연결된 모든 조건문의 조건이 맞을 때
+//	// 가장 위에 있는 조건문만 실행됩니다.
+#pragma endregion
 
-	int integer = 0;
+#pragma region switch문
+//	// 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
+//
+//	char grade = 'H';
+//
+//	// switch문은 해당 조건이 완료되었을 떄, break문이 없으면
+//	// 나머지 밑에 있는 case 문까지 계속 실행시키다가 종료합니다.
+//
+//	switch (grade)
+//	{
+//		case 'A': printf("100 ~ 90\n");
+//			break;
+//		case 'B': printf("89 ~ 80\n");
+//			break;
+//		case 'C': printf("79 ~ 70\n");
+//			break;
+//		case 'D': printf("69 ~ 60\n");
+//			break;
+//		case 'F': printf("60 미만\n");
+//			break;
+//		default : printf("예외\n");
+//			break;
+//	}
+//
+//	// switch문의 경우 조건에 해당하는 값에 따라 조건의
+//	// 위치로 이동합니다.
+#pragma endregion
 
-	if (integer > 0) {
-		printf("positive number");
-	}
-	else if (integer < 0) {
-		printf("negative quantity");
-	}
-	else
-	{
-		printf("integer");
-	}
+#pragma region  논리 연산자
 
-	// if문에 연결된 모든 조건문의 조건이 맞을 때
-	// 가장 위에 있는 조건문만 실행됩니다.
+#pragma region  AND 연산자
+//	// 두 개의 조건이 다 성립될 때 실행되는 연산자입니다.
+//
+//	int score = 60;
+//	int attendance = 80;
+//
+//	if (score >= 60 && attendance >= 80) {
+//	printf("Graduate");
+//	}
+//
+//	// 조건문에서 하나 이상의 조건이 있다면 왼쪽부터 조건을 검사합니다.
+#pragma endregion
+
+#pragma region OR 연산자
+//		// 두 개의 조건 중에서 하나라도 조건이 성립될 떄 실행되는 연산자입니다.
+//	
+//	int level = 60;
+//	int ATK = 70;
+//	
+//	if (level >= 60 || ATK >= 100) {
+//		printf("enter Dungeon");
+// }
+//
+//	// 조건문의 논리 표현식을 평가하는 도중에 결과가 이미 확정이 났다면, 그 이후의 평가는 생략합니다.
+#pragma endregion
+
+#pragma region NOT 연산자
+//	// 하나의 조건을 반전시키는 연산자입니다.
+//
+//int power = 0;
+//
+//if (!power) {
+//	printf(" power on");
+//}
+//else {
+//	printf(" power off");
+//}
+#pragma endregion
+
+
+
+#pragma endregion
+
+#pragma region 사분면
+
+int x = 0;
+int y = 0;
+
+if (x > 0 && y > 0) {
+	printf("제 1 사분면");
+}
+else if (x < 0 && y > 0) {
+	printf("제 2 사분면");
+}
+else if (x < 0  && y < 0 ) {
+	printf("제 3 사분면");
+}
+if (x > 0 && y < 0 ) {
+	printf("제 4 사분면");
+}
+else
+{
+	printf("x, y가 어느 사분면에도 속하지 않습니다");
+}
+
 #pragma endregion
 
 #pragma endregion
